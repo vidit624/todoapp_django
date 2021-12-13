a5ec801398345c911b2fc4c1b83153e1bde09038
vidit624 <gpta.vidit1234@iitkgp.ac.in>
2021-12-14 00:30:35 +0530

    Added forms and Implemented button tasks

--- todolist_app/views.py
+++ todolist_app/views.py
@@ -1,8 +1,33 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from .models import todolist
+from .forms import Todolist_appForm
+from django.views.decorators.http import require_POST
 
 # Create your views here.
 def index(request):
     todo_items = todolist.objects.order_by('id')
-    context = {'todo_items': todo_items}
+    form = Todolist_appForm()
+    context = {'todo_items': todo_items, 'form': form}
     return render(request, 'todolist_app/index.html', context)
+
+@require_POST
+def addTodoItem(request):
+    form = Todolist_appForm(request.POST)
+    if form.is_valid():
+        new_todo = todolist(text= request.POST['text'])
+        new_todo.save()
+    return redirect(index)
+
+def completedTodo(request, todo_id):
+    todo = todolist.objects.get(pk=todo_id)
+    todo.completed = True
+    todo.save()
+    return redirect('index')
+
+def deleteCompleted(request):
+    todolist.objects.filter(completed__exact= True).delete()
+    return redirect('index')
+
+def deleteAll(request):
+    todolist.objects.all().delete()
+    return redirect('index')
